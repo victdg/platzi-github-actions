dd2719f69cf43f0fd6a36d99a20996e3e9dddfdd
victor zubiaga b <vic.tdg@gmail.com>
2025-12-12 07:57:00 -0500

    trigger yml modificado

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { v4 } from "uuid";
 console.log(`Hola mundo desde pipeline,\nuuid: ${v4()}`);
 console.log(`process.env::>> ${JSON.stringify(process.env)}`);
-if (process.env.IS_OK !== "true") throw new Error("Error en el pipeline");
+if (process.env.IS_OK !== "true")
+  throw new Error(`El estado de IS_OK::>>${process.env.IS_OK}`);
